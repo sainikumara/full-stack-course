5efd9db31ebc11cba2c4f3e6f009c27e79af3b97
Kumara Saini M <saini@iki.fi>
2018-01-29 02:15:13 +0200

    tehtävä 1.12

---
+++ anecdotes/src/index.js
@@ -0,0 +1,48 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      selected: 0
+    }
+  }
+
+  changeSelected = () => {
+    this.setState({
+      selected: Math.floor(Math.random() * 6)
+    })
+  }
+
+  render() {
+    return (
+      <div>
+        {this.props.anecdotes[this.state.selected]}
+        <Button action={this.changeSelected} />
+      </div>
+    )
+  }
+}
+
+const Button = (props) => {
+  return (
+    <div>
+      <button onClick={props.action}> next anecdote </button>
+    </div>
+  )
+}
+
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
+]
+
+ReactDOM.render(
+  <App anecdotes={anecdotes} />,
+  document.getElementById('root')
+)
