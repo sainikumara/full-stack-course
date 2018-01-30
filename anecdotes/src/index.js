c13959d0f456728d0a4763509045760274a488b9
Kumara Saini M <saini@iki.fi>
2018-01-30 21:11:36 +0200

    tehtävät 1.13 ja 1.14

--- anecdotes/src/index.js
+++ anecdotes/src/index.js
@@ -5,7 +5,8 @@ class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      selected: 0
+      selected: 0,
+      votes: [0, 0, 0, 0, 0, 0]
     }
   }
 
@@ -15,20 +16,63 @@ class App extends React.Component {
     })
   }
 
+  voteForSelected = () => {
+    let votesArray = this.state.votes
+    votesArray[this.state.selected] += 1
+      
+    this.setState({
+      votes: [...votesArray]      
+    })
+  }
+
+  mostVotes = () =>  {
+    let votesArray = this.state.votes
+    
+    let maxVotes = Math.max(...votesArray)
+    let index = votesArray.indexOf(maxVotes)
+    
+    return [index, maxVotes]
+  }
+
   render() {
+    let mostVotedIndexAndVotes = this.mostVotes()
+      
     return (
       <div>
         {this.props.anecdotes[this.state.selected]}
-        <Button action={this.changeSelected} />
+        <Votes numberOfVotes={this.state.votes[this.state.selected]} />
+        <Button action={this.voteForSelected} text='vote' />
+        <Button action={this.changeSelected} text='next anecdote' />
+        <MostVotes 
+          anecdote={this.props.anecdotes[mostVotedIndexAndVotes[0]]}
+          maxVotes={mostVotedIndexAndVotes[1]} />
       </div>
     )
   }
 }
 
+const Votes = (props) => {
+  return (
+    <div>
+      <p>has {props.numberOfVotes} votes</p>
+    </div>
+  )
+}
+
 const Button = (props) => {
   return (
     <div>
-      <button onClick={props.action}> next anecdote </button>
+      <button onClick={props.action}> {props.text} </button>
+    </div>
+  )
+}
+
+const MostVotes = (props) => {
+  return (
+    <div>
+      <h1>Anecdote with most votes:</h1>
+      <p>{props.anecdote}</p>
+      <p>It has {props.maxVotes} votes.</p>
     </div>
   )
 }
