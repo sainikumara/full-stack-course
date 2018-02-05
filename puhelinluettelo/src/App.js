53429855cfd949ca59c218faaf64048a725d026b
Kumara Saini M <saini@iki.fi>
2018-02-06 00:23:07 +0200

    tehtävä 2.19, typo korjattu

--- puhelinluettelo/src/App.js
+++ puhelinluettelo/src/App.js
@@ -27,7 +27,7 @@ class App extends React.Component {
       })
   }
 
-  tryAddinAPerson = (event) => {
+  tryAddingAPerson = (event) => {
     event.preventDefault()
 
     if (this.state.persons.every(
